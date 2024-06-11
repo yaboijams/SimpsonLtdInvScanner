a716b28cd8fc787c1bed38e636892835e09db0e2
Jamari Mitchell <92458788+yaboijams@users.noreply.github.com>
2024-06-11 05:22:49 -0500

    nighty night

--- app/src/main/java/com/example/simpsonltdinvscanner/ScanActivity.kt
+++ app/src/main/java/com/example/simpsonltdinvscanner/ScanActivity.kt
@@ -29,7 +29,6 @@ class ScanActivity : AppCompatActivity() {
             Log.d("ScanActivity", "SKU scanned data observed: $scanData")
             findViewById<TextView>(R.id.scanDataTextView).text = scanData
             isScanningLocation = true
-            viewModel.prepareForNextScan()
         })
 
         viewModel.errorMessage.observe(this, Observer { errorMessage ->
@@ -38,13 +37,47 @@ class ScanActivity : AppCompatActivity() {
                 visibility = View.VISIBLE
             }
         })
+
+        viewModel.roomDescription.observe(this, Observer { description ->
+            findViewById<TextView>(R.id.roomDescriptionTextView).text = description
+        })
+
+        viewModel.category.observe(this, Observer { category ->
+            findViewById<TextView>(R.id.categoryTextView).text = category
+        })
+
+        viewModel.subcategory.observe(this, Observer { subcategory ->
+            findViewById<TextView>(R.id.subcategoryTextView).text = subcategory
+        })
+
+        viewModel.subcategoryType.observe(this, Observer { subcategoryType ->
+            findViewById<TextView>(R.id.subcategoryTypeTextView).text = subcategoryType
+        })
+
+        viewModel.action.observe(this, Observer { action ->
+            findViewById<TextView>(R.id.actionTextView).text = action
+        })
+
+        viewModel.caliber.observe(this, Observer { caliber ->
+            findViewById<TextView>(R.id.caliberTextView).text = caliber
+        })
+
+        viewModel.license.observe(this, Observer { license ->
+            findViewById<TextView>(R.id.licenseTextView).text = license
+        })
+
+        viewModel.title.observe(this, Observer { title ->
+            findViewById<TextView>(R.id.titleTextView).text = title
+        })
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         Log.d("ScanActivity", "Key down event: keyCode = $keyCode")
-        if (keyCode == 103 || keyCode == 10036) {
-            viewModel.triggerScanner(isScanningLocation)
-            return true
+        when (keyCode) {
+            103, 10036 -> {
+                viewModel.triggerScanner(isScanningLocation)
+                return true
+            }
         }
         return super.onKeyDown(keyCode, event)
     }
@@ -57,4 +90,3 @@ class ScanActivity : AppCompatActivity() {
         return super.onKeyUp(keyCode, event)
     }
 }
-
--- app/src/main/java/com/example/simpsonltdinvscanner/ScanViewModel.kt
+++ app/src/main/java/com/example/simpsonltdinvscanner/ScanViewModel.kt
@@ -25,12 +25,35 @@ class ScanViewModel(application: Application) : AndroidViewModel(application), E
     private val _skuScanData = MutableLiveData<String>()
     val skuScanData: LiveData<String> get() = _skuScanData
 
-    private var isScanningLocation = true
+    private val _roomDescription = MutableLiveData<String>()
+    val roomDescription: LiveData<String> get() = _roomDescription
+
+    private val _category = MutableLiveData<String>()
+    val category: LiveData<String> get() = _category
+
+    private val _subcategory = MutableLiveData<String>()
+    val subcategory: LiveData<String> get() = _subcategory
+
+    private val _subcategoryType = MutableLiveData<String>()
+    val subcategoryType: LiveData<String> get() = _subcategoryType
+
+    private val _action = MutableLiveData<String>()
+    val action: LiveData<String> get() = _action
+
+    private val _caliber = MutableLiveData<String>()
+    val caliber: LiveData<String> get() = _caliber
+
+    private val _license = MutableLiveData<String>()
+    val license: LiveData<String> get() = _license
+
+    private val _title = MutableLiveData<String>()
+    val title: LiveData<String> get() = _title
+
     private var currentLocation: String? = null
 
     private val db = FirebaseFirestore.getInstance()
-
     private val locationsCollection = db.collection("Locations")
+    private val algoliaCollection = db.collection("Algolia")
 
     private val _errorMessage = MutableLiveData<String>()
     val errorMessage: LiveData<String> get() = _errorMessage
@@ -57,7 +80,6 @@ class ScanViewModel(application: Application) : AndroidViewModel(application), E
         }
     }
 
-
     override fun onOpened(emdkManager: EMDKManager) {
         this.emdkManager = emdkManager
         Log.d("ScanViewModel", "EMDK Manager opened successfully")
@@ -66,7 +88,6 @@ class ScanViewModel(application: Application) : AndroidViewModel(application), E
         Log.d("ScanViewModel", "BarcodeManager initialized successfully")
         initScanner()
     }
-
 
     private fun initScanner() {
         try {
@@ -91,7 +112,6 @@ class ScanViewModel(application: Application) : AndroidViewModel(application), E
         try {
             if (isScannerInitialized) {
                 Log.d("ScanViewModel", "Scanner triggered")
-                this.isScanningLocation = isScanningLocation
                 scanner.read()
             } else {
                 Log.e("ScanViewModel", "Scanner is not initialized")
@@ -100,7 +120,6 @@ class ScanViewModel(application: Application) : AndroidViewModel(application), E
             Log.e("ScanViewModel", "Error triggering scanner: ${e.message}")
         }
     }
-
 
     fun prepareForNextScan() {
         if (::scanner.isInitialized) {
@@ -121,69 +140,105 @@ class ScanViewModel(application: Application) : AndroidViewModel(application), E
             for (scanData in scanDataCollection.scanData) {
                 val scannedData = scanData.data
                 Log.d("ScanViewModel", "Received scan data: $scannedData")
-                if (isScanningLocation) {
-                    val isValidLocation = isValidLocation(scannedData)
-                    if (isValidLocation) {
-                        currentLocation = scannedData
-                        _locationScanData.postValue(scannedData)
-                    } else {
-                        Log.d("ScanViewModel", "Invalid location: $scannedData")
-                        _errorMessage.postValue("Invalid location: $scannedData")
-                    }
-                } else {
-                    _skuScanData.postValue(scannedData)
-                    saveScanDataToFirebase(currentLocation ?: "unknown_location", scannedData)
-                }
+                checkLocationValidityOrSku(scannedData)
             }
         }
     }
 
-    private fun isValidLocation(location: String): Boolean {
-        var isValid = false
+    private fun checkLocationValidityOrSku(data: String) {
         locationsCollection.get()
             .addOnSuccessListener { documents ->
+                var isLocation = false
+                var description = ""
                 for (document in documents) {
                     val locCode = document.getString("locCode")
-                    if (locCode != null && location.contains(locCode)) {
-                        isValid = true
+                    val locDescription = document.getString("description")
+                    if (locCode != null && data.startsWith(locCode)) {
+                        isLocation = true
+                        description = locDescription ?: "No description available"
                         break
                     }
                 }
+                if (isLocation) {
+                    currentLocation = data
+                    _locationScanData.postValue(data)
+                    _roomDescription.postValue(description)
+                } else {
+                    _skuScanData.postValue(data)
+                    fetchAdditionalData(data)
+                    saveScanDataToFirebase(currentLocation, data)
+                }
+                prepareForNextScan()
             }
             .addOnFailureListener { exception ->
                 Log.e("ScanViewModel", "Error getting documents: ", exception)
+                _errorMessage.postValue("Error checking location: ${exception.message}")
             }
-        return isValid
     }
 
-    private fun saveScanDataToFirebase(location: String, sku: String) {
+    private fun fetchAdditionalData(sku: String) {
+        algoliaCollection.document(sku).get()
+            .addOnSuccessListener { document ->
+                if (document != null && document.exists()) {
+                    _category.postValue(document.getString("Category") ?: "Data not available")
+                    _subcategory.postValue(document.getString("Subcategory") ?: "Data not available")
+                    _subcategoryType.postValue(document.getString("SubcategoryType") ?: "Data not available")
+                    _action.postValue(document.getString("Action") ?: "Data not available")
+                    _caliber.postValue(document.getString("Caliber") ?: "Data not available")
+                    _license.postValue(document.getString("License") ?: "Data not available")
+                    _title.postValue(document.getString("Title") ?: "Data not available")
+                } else {
+                    _category.postValue("Data not available")
+                    _subcategory.postValue("Data not available")
+                    _subcategoryType.postValue("Data not available")
+                    _action.postValue("Data not available")
+                    _caliber.postValue("Data not available")
+                    _license.postValue("Data not available")
+                    _title.postValue("Data not available")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.e("ScanViewModel", "Error fetching additional data: ", exception)
+                _category.postValue("Data not available")
+                _subcategory.postValue("Data not available")
+                _subcategoryType.postValue("Data not available")
+                _action.postValue("Data not available")
+                _caliber.postValue("Data not available")
+                _license.postValue("Data not available")
+                _title.postValue("Data not available")
+            }
+    }
+
+    private fun saveScanDataToFirebase(location: String?, sku: String) {
+        if (location == null) {
+            Log.e("ScanViewModel", "Location is null. Cannot save scan data.")
+            return
+        }
+
         val scanData = hashMapOf("sku" to sku, "timestamp" to System.currentTimeMillis())
         db.collection("scanner")
             .document(location)
             .collection("skus")
             .add(scanData)
             .addOnSuccessListener { documentReference ->
-                Log.d("ScanViewModel", "DocumentSnapshot added with ID: ${documentReference.id}")
+                Log.d("ScanViewModel", "DocumentSnapshot written with ID: ${documentReference.id}")
             }
             .addOnFailureListener { e ->
                 Log.w("ScanViewModel", "Error adding document", e)
+                _errorMessage.postValue("Error saving scan data: ${e.message}")
             }
     }
 
     override fun onClosed() {
         if (::emdkManager.isInitialized) {
             emdkManager.release()
-            Log.d("ScanViewModel", "EMDK Manager released")
-        } else {
-            Log.e("ScanViewModel", "EMDK Manager was not initialized")
         }
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        if (::barcodeManager.isInitialized) {
-            emdkManager.release(EMDKManager.FEATURE_TYPE.BARCODE)
-            Log.d("ScanViewModel", "Barcode Manager released")
+    fun onDestroy() {
+        if (::emdkManager.isInitialized) {
+            emdkManager.release()
         }
+        super.onCleared()
     }
 }
